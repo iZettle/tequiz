459e1af28a95ed8f4189e285799310a62740afc1
Xiao Zhang <zhangxiao@xzhang.net>
2023-05-21 22:06:31 +0200

    Update README

--- README.md
+++ README.md
@@ -4,3 +4,27 @@ Tequiz
 ```
 merge(tetris, quiz)
 ```
+
+Tetris was the very first programming project I finished back in 2002-2003.
+Back then I used C# and it was a GUI application running in Windows.
+
+Recently I watched the movie [Tetris]. When I saw tetrominoes are rendered by
+pair of `[]`, I said: Beautiful.
+
+So, after 20 years, I decided to implement the game one more time. This time I
+make it a TUI application, and of course, each tetromino is rendered as `[]` by
+default:)
+
+However, worth noting that:
+
+> The game has monochrome graphics, and in the first revision of the game, the
+> blocks in the tetrominos are represented by a pair of delete/rubout
+> characters (character code 177); however, the rendering of this character
+> code as a rectangle was a feature specific to the Soviet clone machines, an
+> actual PDP-11 would instead display nothing. A later revision was made where
+> the blocks are represented by a pair of square brackets instead.
+>
+> -- <cite>https://tetris.wiki/Tetris_(Electronika_60)</cite>
+
+
+[Tetris]: https://en.wikipedia.org/wiki/Tetris_(film)
--- src/grid.rs
+++ src/grid.rs
@@ -6,11 +6,11 @@ pub const WIDTH: u8 = 10;
 pub const HEIGHT: u8 = 21;
 
 #[derive(Clone)]
-pub struct Block {
+pub struct Tetromino {
     rotations: [[i16; 4]; 4],
 }
 
-impl Block {
+impl Tetromino {
     pub fn get_cells(&self, position: u8, rotation: u8) -> [i16; 4] {
         let mut cells: [i16; 4] = [0; 4];
         for i in 0..4 {
@@ -21,13 +21,13 @@ impl Block {
     }
 }
 
-const BLOCK_VARIANT: usize = 2;
-const BLOCKS: [Block; BLOCK_VARIANT] = [
+const TETROMINO_VARIANT: usize = 2;
+const TETROMINOS: [Tetromino; TETROMINO_VARIANT] = [
     //           []
     // []()[][]  ()
     //           []
     //           []
-    Block {
+    Tetromino {
         rotations: [
             [-1, 0, 1, 2],
             [0 - WIDTH as i16, 0, WIDTH as i16, (WIDTH * 2) as i16],
@@ -38,7 +38,7 @@ const BLOCKS: [Block; BLOCK_VARIANT] = [
 
     // ()[]
     // [][]
-    Block {
+    Tetromino {
         rotations: [
             [0, 1, WIDTH as i16, (WIDTH + 1) as i16],
             [0, 1, WIDTH as i16, (WIDTH + 1) as i16],
@@ -51,7 +51,7 @@ const BLOCKS: [Block; BLOCK_VARIANT] = [
 pub struct Grid {
     pub cells: [bool; (WIDTH * HEIGHT) as usize],
 
-    block_id: Option<usize>,
+    tetromino_id: Option<usize>,
     position: u8,
     rotation: u8,
     rate: Duration,
@@ -64,7 +64,7 @@ impl Grid {
     pub fn new(rate: Duration) -> Grid {
         Grid {
             cells: [false; (WIDTH * HEIGHT) as usize],
-            block_id: None,
+            tetromino_id: None,
             position: 0,
             rotation: 0,
             rate,
@@ -79,22 +79,65 @@ impl Grid {
         if self.timer >= self.rate {
             self.timer = self.timer - self.rate;
 
-            match self.block_id {
+            match self.tetromino_id {
                 Some(_) => self.fall(),
-                None => self.next_block(),
+                None => self.next_tetromino(),
             }
         }
     }
 
     fn fall(&mut self) {
         if !self.move_if_can(self.position + WIDTH) {
-            self.next_block();
+            self.clear();
+            self.next_tetromino();
+        }
+    }
+
+    fn clear(&mut self) {
+        let mut finished: [bool; HEIGHT as usize] = [true; HEIGHT as usize];
+        for y in 0..HEIGHT {
+            for x in 0..WIDTH {
+                if !self.cells[(y * WIDTH + x) as usize] {
+                    finished[y as usize] = false;
+                    break;
+                }
+            }
+        }
+
+
+        let mut y = 0;
+        loop {
+            if finished[y as usize] {
+                print!("{}{}", cursor::Goto(1, 1), y);
+                let mut yy = y;
+                loop {
+                    for x in 0..WIDTH {
+                        if yy == 0 {
+                            self.cells[(yy * WIDTH + x) as usize] = false;
+                        } else {
+                            self.cells[(yy * WIDTH + x) as usize] = self.cells[((yy - 1) * WIDTH + x) as usize];
+                        }
+                    }
+
+                    if yy == 0 {
+                        break;
+                    }
+
+                    yy = yy - 1;
+                }
+            }
+
+            if y == HEIGHT - 1 {
+                break;
+            }
+
+            y = y + 1;
         }
     }
 
     pub fn horizontal_move(&mut self, offset: i8) {
-        if let Some(block_id) = self.block_id {
-            let current = BLOCKS[block_id].get_cells(self.position, self.rotation);
+        if let Some(tetromino_id) = self.tetromino_id {
+            let current = TETROMINOS[tetromino_id].get_cells(self.position, self.rotation);
             let mut can_move = true;
             for i in 0..current.len() {
                 if current[i] < 0 {
@@ -117,9 +160,9 @@ impl Grid {
     }
 
     fn move_if_can(&mut self, new_position: u8) -> bool {
-        let block_id = self.block_id.unwrap();
-        let current = BLOCKS[block_id].get_cells(self.position, self.rotation);
-        let after = BLOCKS[block_id].get_cells(new_position, self.rotation);
+        let tetromino_id = self.tetromino_id.unwrap();
+        let current = TETROMINOS[tetromino_id].get_cells(self.position, self.rotation);
+        let after = TETROMINOS[tetromino_id].get_cells(new_position, self.rotation);
 
         let mut can_move = true;
 
@@ -133,9 +176,8 @@ impl Grid {
                 break;
             }
 
-            print!("{}{}", cursor::Goto(1,1), after[i] as usize);
             if self.cells[after[i] as usize] {
-                // check if this cell is in the block itself
+                // check if this cell is in the tetromino itself
                 let mut is_self = false;
                 for j in 0..current.len() {
                     if after[i] == current[j] {
@@ -169,9 +211,9 @@ impl Grid {
         return false;
     }
 
-    fn next_block(&mut self) {
-        let n = self.rng.gen_range(0..BLOCK_VARIANT);
-        self.block_id = Some(n);
+    fn next_tetromino(&mut self) {
+        let n = self.rng.gen_range(0..TETROMINO_VARIANT);
+        self.tetromino_id = Some(n);
         self.rotation = 0;
         self.reset_position();
     }
